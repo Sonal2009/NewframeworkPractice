50b4a91e2b07d3e6958285f8852ac33f24c8cfd2
Sonal Patel <sonaldisharohit@gmail.com>
2019-05-17 16:28:52 -0700

    frameworkRepo

---
+++ src/test/java/TestCases/ApiTest.java
@@ -0,0 +1,36 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class ApiTest {
+
+	
+	@Test
+	public void postjira() {
+		
+		System.out.println("postjira");
+		
+	}
+	
+	@Test
+	
+	public void DeleteTwitter() {
+		
+		
+		System.out.println("DeleteTwitter");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/TestCases/AppiumTest.java
@@ -0,0 +1,32 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeApp() {
+		
+		System.out.println("NativeApp");
+		
+	}
+	
+	@Test
+	
+	public void IosAppr() {
+		
+		System.out.println("IosAppr");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
